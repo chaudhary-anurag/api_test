1146ff87f1c982423b0adac704592a1147eb93f4
anurag <anuragatservice@gmail.com>
2017-11-18 00:37:23 +0530

    New Product created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
 use App\Product;
@@ -9,6 +9,10 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -35,9 +39,19 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = new Product;
+        $product->name = $request->name;
+        $product->detail=$request->description;
+        $product->stock= $request->stock;
+        $product->price= $request->price;
+        $product->discount=$request->discount;
+        $product->save();
+        return response([
+            'data' => new ProductResource($product)
+            ],201);
+    
     }
 
     /**
